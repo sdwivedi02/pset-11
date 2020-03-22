a300a6dc3dbe185b2a1a11fb4265cd96dde65b84
Sambhavi Dwivedi <sambhavi@Sambhavis-MBP.hsd1.nj.comcast.net>
2020-03-22 17:43:11 -0400

    bubble finished

--- src/Exercises.java
+++ src/Exercises.java
@@ -89,6 +89,15 @@ public class Exercises {
 				}
 			}
 		}
+    if (!ascending) {
+			int i, t;
+			int n = list.length;
+			for (i = 0; i < n / 2; i++) {
+				t = list[i];
+				list[i] = list[n - i - 1];
+				list[n - i - 1] = t;
+			}
+		}
     return list;
   }
 
